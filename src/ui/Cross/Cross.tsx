17b2816d75bf228121a6a3b63aa430d02a2766c6
geekayush1812 <ayushtripathi9794@gmail.com>
2022-01-20 19:08:44 +0530

    fix: svg ios and safari issue

--- src/ui/Cross/Cross.tsx
+++ src/ui/Cross/Cross.tsx
@@ -14,7 +14,7 @@ const Cross = () => {
         }
     },[hasSound])
     return (
-        <svg className='cr87crossSvgContainer' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <svg className='cr87crossSvgContainer' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" version="1.1">
             <line className='cr87lineDiagonal1' x1="20" y1="20" x2="80" y2="80" stroke="#7D64BD" strokeWidth="6" strokeDasharray="145" strokeDashoffset="145" />
             <line className='cr28lineDiagonal2' x1="80" y1="20" x2="20" y2="80" stroke="#7D64BD" strokeWidth="6" strokeDasharray="145" strokeDashoffset="145"/>
         </svg>
